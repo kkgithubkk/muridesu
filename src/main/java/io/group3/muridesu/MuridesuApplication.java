e6dd0d047eb90790612f6e4093ed020b8bc1c493
kensuke.konishi-at-227708528139 <kensuke.konishi@geekplus.co.jp>
2023-04-16 20:49:45 +0900

    feat: substancially first commit

---
+++ src/main/java/io/group3/muridesu/MuridesuApplication.java
@@ -0,0 +1,13 @@
+package io.group3.muridesu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuridesuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuridesuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/group3/muridesu/MuridesuApplicationTests.java
@@ -0,0 +1,13 @@
+package io.group3.muridesu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuridesuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
